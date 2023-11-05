71730d103f7591a1e41d39fe42c4d3c28cc913a0
Pavlo Izhytskyi <kn1b21.izhytskyi@kpnu.edu.ua>
2023-11-05 12:33:11 +0200

    update

--- lab01/lib/main.dart
+++ lab01/lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+class CarDetails {
+  final String name;
+  final int year;
+  final double price;
+  final String imageAsset;
+
+  CarDetails(this.name, this.year, this.price, this.imageAsset);
+}
+
+final car = CarDetails('Назва автомобіля', 2023, 25000, 'assets/car_image.jpg');
+
 void main() {
   runApp(CarListingApp());
 }
@@ -21,50 +32,47 @@ class CarListingApp extends StatelessWidget {
 class CarListing extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Image.asset(
-          'assets/car_image.jpg',
-           height: 200, // Встановіть бажану висоту для зображення
-            fit: BoxFit.contain, // Масштабувати зображення так, щоб воно вмістилося у вказаній висоті
-),
-          SizedBox(height: 20),
-          Text(
-            'Назва автомобіля',
-            style: TextStyle(fontSize: 24),
-          ),
-          Text(
-            'Рік випуску: 2023',
-            style: TextStyle(fontSize: 18),
-          ),
-          Text(
-            'Ціна: \$25,000',
-            style: TextStyle(fontSize: 18),
-          ),
-          Container(
-            margin: EdgeInsets.all(20),
-            width: double.infinity,
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.blue, Colors.green],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-              borderRadius: BorderRadius.circular(10),
+    return ListView(
+      padding: EdgeInsets.all(16),
+      children: [
+        Image.asset(
+          car.imageAsset,
+          height: MediaQuery.of(context).size.height * 0.3,
+          fit: BoxFit.contain,
+        ),
+        SizedBox(height: 20),
+        Text(
+          car.name,
+          style: TextStyle(fontSize: 24),
+        ),
+        Text(
+          'Рік випуску: ${car.year}',
+          style: TextStyle(fontSize: 18),
+        ),
+        Text(
+          'Ціна: \$${car.price.toStringAsFixed(2)}',
+          style: TextStyle(fontSize: 18),
+        ),
+        Container(
+          margin: EdgeInsets.all(20),
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue, Colors.green],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-            child: Text(
-              'Зараз у продажу! Звертайтеся за телефоном +1234567890',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Text(
+            'Зараз у продажу! Звертайтеся за телефоном +1234567890',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
